291f23a874f620d4bd3f4a89abec0bb206f2adcc
Wesleynicodemos <Wesleyn1373@hotmail.com>
2024-08-27 18:25:54 -0300

    criação de um botão hamburger.

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.menu-icon')
+
+menu.addEventListener('click',()=>{
+    const menu = document.querySelector('.menu-icon')
+    menu.classList.toggle('active')
+})
